4f20095b710e51cb459d9eaca4530f90f369a737
raidzero <paul.raidzero@gmail.com>
2011-12-06 17:58:20 -0700

    hopefully fix emmc and external sdcard for xoom

---
+++ a500/BoardConfig.mk
@@ -0,0 +1,32 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/raidzero/a500/BoardConfigVendor.mk
+
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_BOOTLOADER_BOARD_NAME := a500
+
+ARCH_ARM_HAVE_TLS_REGISTER := true
+NEEDS_ARM_ERRATA_754319_754320 := true
+TARGET_GLOBAL_CFLAGS += -DNEEDS_ARM_ERRATA_754319_754320
+
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/raidzero/a500/kernel
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
