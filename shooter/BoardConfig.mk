6a73503fe0203bef645c6ccce047a7cc9b5c927f
raidzero <paul.raidzero@gmail.com>
2011-12-18 20:12:55 -0700

    add galaxy nexus, evo3d, change a few things for some other things

--- shooter/BoardConfig.mk
+++ shooter/BoardConfig.mk
@@ -5,11 +5,11 @@ USE_CAMERA_STUB := true
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
+TARGET_CPU_ABI := armeabi-v7a
 TARGET_BOOTLOADER_BOARD_NAME := shooter
 
-BOARD_KERNEL_CMDLINE := no_console_suspend=1
-BOARD_KERNEL_BASE := 0x40400000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=shooter no_console_suspend=1
+BOARD_KERNEL_BASE := 0x48000000
 BOARD_KERNEL_PAGESIZE := 2048
 
 # fix this up by examining /proc/mtd on a running device
@@ -17,6 +17,12 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/raidzero/shooter/kernel
 
+BOARD_CUSTOM_GRAPHICS           := ../../../device/raidzero/olympus/recovery/graphics.c
 #BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
+#BOARD_HAS_INVERTED_VOLUME := true
+
+#HTC OVERLAY
+include device/raidzero/htc-overlay/BoardConfig.mk
+
--- toro/device_toro.mk
+++ toro/device_toro.mk
@@ -3,13 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/raidzero/shooter/shooter-vendor.mk)
+$(call inherit-product-if-exists, vendor/raidzero/toro/toro-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/raidzero/shooter/overlay
+DEVICE_PACKAGE_OVERLAYS += device/raidzero/toro/overlay
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/raidzero/shooter/kernel
+	LOCAL_KERNEL := device/raidzero/toro/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_shooter
-PRODUCT_DEVICE := shooter
+PRODUCT_NAME := full_toro
+PRODUCT_DEVICE := toro
