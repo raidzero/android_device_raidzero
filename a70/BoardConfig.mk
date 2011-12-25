4b0c35b7edd765b2437c5ab79205e46ec2d480e2
raidzero <paul.raidzero@gmail.com>
2011-12-24 22:13:57 -0700

    Add a70 and skyrocket

---
+++ a70/BoardConfig.mk
@@ -0,0 +1,23 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/raidzero/a70/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := a70
+
+BOARD_KERNEL_CMDLINE := mem=212M console=ttyMSM2 androidboot.hardware=qcom
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/raidzero/a70/kernel
+
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0"
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true#BOARD_HAS_INVERTED_VOLUME := true
